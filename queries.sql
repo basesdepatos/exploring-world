2b5fd42d841683fbdb0485bbae0b2fae6217cd10
Karls3fni <m.viladorado@gmail.com>
2025-02-03 13:40:40 +0100

    added table of all cities in Germany along with their population

--- queries.sql
+++ queries.sql
@@ -11,3 +11,7 @@ FROM
     countrylanguage
 WHERE
     country.Code = countrylanguage.CountryCode;
+SELECT city.name, city.population
+FROM city
+INNER JOIN country 
+WHERE country.name = "Germany" AND city.countrycode = country.code;
